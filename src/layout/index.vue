30569f11f9a91e720d01e2eee9d650377cdc1d43
Rellopn <614046330@qq.com>
2021-12-09 15:54:44 +0800

    布局ok

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,14 @@
 <template>
   <el-container class="el-container-main">
-
-    <el-aside class="el-aside-style">
-      <LayoutSidebar></LayoutSidebar>
-    </el-aside>
-
+    <el-header class="el-header-style">
+      <LayoutHeader></LayoutHeader>
+    </el-header>
     <el-container>
-      <el-header class="el-header-style">
-        <LayoutHeader></LayoutHeader>
-      </el-header>
+      <el-aside class="el-aside-style">
+        <LayoutSidebar></LayoutSidebar>
+      </el-aside>
 
-      <el-main>
+      <el-main class="el-main-style" :style="{height:bindData.height}">
         <el-table :data="tableData">
           <el-table-column prop="date" label="Date" width="140">
           </el-table-column>
@@ -24,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue'
+import {defineComponent, ref,onBeforeMount} from 'vue'
 import LayoutHeader from './Header/index.vue'
 import LayoutSidebar from './Sidebar/index.vue'
 
@@ -34,6 +32,9 @@ export default defineComponent({
     LayoutSidebar
   },
   setup() {
+    let bindData = {
+      height : ""
+    }
     const item = {
       date: '2016-05-02',
       name: 'Tom',
@@ -42,14 +43,20 @@ export default defineComponent({
 
     const tableData = ref(Array(20).fill(item))
 
+    onBeforeMount(()=>{
+      bindData.height = (document.documentElement.clientHeight-65)+'px';
+    })
+
     return {
       tableData,
+      bindData
     }
   },
 })
 </script>
 
 <style>
+
 .el-header {
   background-color: #b3c0d1;
   color: var(--el-text-color-primary);
@@ -58,20 +65,25 @@ export default defineComponent({
 
 .el-aside {
   color: var(--el-text-color-primary);
+  --el-aside-width:15vw!important;
 }
 
 .el-container-main {
-  height: 500px;
-  border: 1px solid #eee
+  border: 1px solid #eee;
+
 }
 
 .el-aside-style {
-  width: 200px;
+  --el-aside-width: 150px;
   background-color: rgb(238, 241, 246)
 }
 
 .el-header-style {
   text-align: right;
   font-size: 12px
+}
+.el-main-style{
+  height: 50vh;
+  overflow-y: scroll;
 }
 </style>
